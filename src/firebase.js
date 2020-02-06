459a63e17ab4f4630398ef11752ff62b09e4194d
ammark47 <ammark47@yahoo.com>
2020-02-06 17:52:42 -0500

    user profile stored

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,14 @@ const config = {
   apiKey: "AIzaSyDvzzq5wOMCdSAMAfSmvErvlQM7Qn-juKs",
   databaseURL: "https://reviewed-c96e4.firebaseio.com",
   projectId: "reviewed-c96e4",
-  messagingSenderId: "85261328553"
+  // messagingSenderId: "85261328553"
 }
 firebase.initializeApp(config);
-const databaseRef = firebase.database().ref();
-export const usersRef = databaseRef.child("userprofiles")
+const databaseRef = firebase.database();
+export const usersRef = databaseRef.ref("users");
+
+
+export function createUserInFirebase(userProfile, userID) {
+    console.log(userProfile, userID)
+    usersRef.child(userID).set(userProfile)
+}
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,10 +2,21 @@ import { takeLatest } from 'redux-saga/effects';
 import { HANDLE_AUTHENTICATION_CALLBACK, USER_PROFILE_LOADED } from '../actions/auth';
 import { handleAuthentication } from '../../Auth0';
 import { all, call, put, takeEvery } from 'redux-saga/effects';
+import { usersRef, createUserInFirebase } from '../../firebase'
 
 export function* parseHash() {
     const user = yield call(handleAuthentication);
     yield put({ type: USER_PROFILE_LOADED, user });
+    console.log(user.profile.sub, usersRef)
+    usersRef.child(user.profile.sub).once('value')
+    .then(snapshot => {
+        console.log('here')
+        if (snapshot.exists()) {
+            console.log('exists', snapshot)
+        } else {
+            createUserInFirebase(user, user.profile.sub)
+        }
+    })
 }
 
 export function* handleAuthenticationCallback() {
