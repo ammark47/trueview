b5ec24266ec5c71dd9578d99710de3902971b21c
ammark47 <ammark47@yahoo.com>
2020-04-30 16:45:17 -0400

    clean up

--- server/db_helpers/user_model.js
+++ server/db_helpers/user_model.js
@@ -1,40 +1,3 @@
-// const Pool = require('pg').Pool
-
-// const pool = new Pool({
-//     user: 'my_user',
-//     host: 'localhost',
-//     database: 'trueview_database',
-//     password: 'root',
-//     port: 5432,
-//   });
-
-//   const getUsers = (request, response) => {
-//     pool.query('SELECT * FROM public.users', (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       console.log('results', results.rows)
-//       response.status(200).json(results.rows)
-//     })
-//   }
-
-// const getUsers = () => {
-//     return new Promise(function(resolve, reject) {
-//         pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-//             if (error) {
-//                 reject(error)
-//                 return
-//             }
-//             if (results === undefined) {
-//                 console.log('rejecting')
-//                 reject('no users')
-//                 return
-//             }
-//             resolve(results.rows)
-//         })
-//     })
-// }
-//
 const pgp = require('pg-promise')();
 const connection = {
     host: 'localhost',
@@ -46,14 +9,6 @@ const connection = {
 }
 const db = pgp(connection);
 
-
-const getUser = async (username) => {
-    return await db.any('SELECT * FROM users WHERE name = $1', [username])     
-}
-
-// const createUser = async (username) => {
-
-// }
 async function getInsertUser(userData) {
     return db.task('getInsertUser', async t => {
         const user = await t.oneOrNone('SELECT * FROM users WHERE name = ${profile.nickname}', userData);
@@ -62,22 +17,7 @@ async function getInsertUser(userData) {
  
 }
 
-// const createUser = (body) => {
-//     return new Promise(function(resolve, reject) {
-//         const { name, email, chatToken } = body
-//         pool.query('INSERT INTO users (name, email, chat_token) VALUES ($1, $2, $3) RETURNING *', [name, email, chatToken], (error, results) => {
-//             if(error){
-//                 reject(error)
-//                 return
-//             }
-//             resolve( `A new user has been added: ${results.rows[0]}` )
-//         })
-//     })
-// }
 
 module.exports = {
-    getUser ,
-    // createUser,
-    // getUser
     getInsertUser
 }
--- server/db_server.js
+++ server/db_server.js
@@ -5,7 +5,6 @@ var bodyParser = require('body-parser')
 
 const userModel = require('./db_helpers/user_model')
 
-// app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
@@ -14,16 +13,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
   });
-
-// app.get('/', (req, res) => {
-//     userModel.getUsers()
-//     .then(response => {
-//         res.status(200).send(response);
-//     })
-//     .catch(error => {
-//         res.status(500).send(error)
-//     })
-// // })
 
 app.post('/user', (req, res) => {    
     userModel.getInsertUser(req.body)
@@ -36,16 +25,6 @@ app.post('/user', (req, res) => {
         res.status(500).send(error)
     })
 })
-
-// app.post('/users', (req, res) => {
-//     userModel.createUser(req.body)
-//     .then( response => {
-//         res.status(200).send(response)
-//     })
-//     .catch( error => {
-//         res.status(500).send(error)
-//     })
-// })
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
