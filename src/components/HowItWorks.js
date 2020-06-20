89ecc1471cae7453b93d492560797030aa8df86a
ammark47 <ammark47@yahoo.com>
2020-06-20 14:59:04 -0400

    reviewer timeline complete

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import GridContainer from 'custom_components/Grid/GridContainer'
 import GridItem from 'custom_components/Grid/GridItem'
 import { CustomerTimeLine } from './CustomerTimeLine'
+import { ReviewerTimeLine } from './ReviewerTimeLine'
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js"
 
@@ -27,16 +28,26 @@ export const HowItWorks = () => {
                     <h2 className={classes.title}>How It Works</h2>
                 </GridItem>
             </GridContainer>
-            <div>
-                <GridContainer direction="column">
-                    <GridItem xs={12} sm={6} md={6}>
-                        <h2 className={classes.subTitle}>Customer</h2>
-                    </GridItem>
-                    <GridItem xs={12} sm={6} md={6}>
-                        <CustomerTimeLine />      
-                    </GridItem>
-                </GridContainer>
-            </div>
+            <GridContainer style={{ flexWrap: 'nowrap' }}>
+                <GridItem xs={12} sm={12} md={12}>
+                    <GridContainer direction="column">
+                        <GridItem xs={12} sm={6} md={6}>
+                            <h2 className={classes.subTitle}>Customer</h2>
+                        </GridItem>
+                        <GridItem xs={12} sm={6} md={6}>
+                            <CustomerTimeLine />      
+                        </GridItem>
+                    </GridContainer>
+                </GridItem>
+                    <GridContainer direction="column" alignItems='flex-end' style={{ width: '100%' }}>
+                        <GridItem xs={12} sm={6} md={6}>
+                            <h2 className={classes.subTitle}>Reviewer</h2>
+                        </GridItem>
+                        <GridItem xs={12} sm={6} md={6}>
+                            <ReviewerTimeLine />      
+                        </GridItem>
+                    </GridContainer>
+            </GridContainer>
         </div>
     )
 }
